262995beda043590a6d3b450d79257619c28f786
Sebastian Gonzalez <onemogen8@gmail.com>
2020-01-30 01:01:48 -0300

    primeras pruebas

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome () {
+    return (
+        <div className='Welcome'>
+
+            <h2 style={{textAlign: 'center'}} >Hello, I am learning React now</h2>
+        </div>
+    )
+}
+
+export default Welcome;
